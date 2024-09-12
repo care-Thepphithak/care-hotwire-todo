4f66a6684cd8bbfdb271d7d9bf13e4adeb92e4cb
e3tow1c <thepphithak.w@kkumail.com>
2024-09-12 16:01:23 +0700

    Update the User and Todo to have the associations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,7 +1,8 @@
 class Todo < ApplicationRecord
+    belongs_to :user
     validates_presence_of :name
     enum status: {
         incomplete: 0,
         complete: 1
-    }    
+    }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :todos, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
