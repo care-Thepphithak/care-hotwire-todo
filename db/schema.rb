31e7aefee9dc66d442ae13b8e5c864040e2cc36e
e3tow1c <thepphithak.w@kkumail.com>
2024-09-12 16:02:10 +0700

    after run peding migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_12_080246) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_12_085832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,5 +20,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_12_080246) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "due_date"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end
