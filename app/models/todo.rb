d84f49466d827e895359bb25493a775d207a4145
e3tow1c <thepphithak.w@kkumail.com>
2024-09-12 17:26:38 +0700

    add send_assignment_email in todo model so an email is being sent when a todo is created

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -5,4 +5,10 @@ class Todo < ApplicationRecord
         incomplete: 0,
         complete: 1
     }
+
+    private
+
+    def send_assignment_email
+        TodoMailer.todo_assigned(self).deliver_now
+    end
 end
