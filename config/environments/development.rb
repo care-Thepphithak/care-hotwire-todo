ee2937291b80d2a5f035bdadb2d60471759f33a3
e3tow1c <thepphithak.w@kkumail.com>
2024-09-12 17:53:11 +0700

    config smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,9 +37,19 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "email-smtp.ap-southeast-1.amazonaws.com",
+    port: 587,
+    domain: "noreply@odds.team",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
