959e4117ec75cff46526a5b3f00cc5f35b75f199
e3tow1c <thepphithak.w@kkumail.com>
2024-09-12 17:25:09 +0700

    add mailer function to send an email

--- app/mailers/todo_mailer.rb
+++ app/mailers/todo_mailer.rb
@@ -1,2 +1,9 @@
 class TodoMailer < ApplicationMailer
+  default from: 'todo@dds.gom'
+
+  def todo_assigned(todo)
+    @todo = todo
+    @user = todo.user
+    mail(to: @user.email, subject: 'You have been assigned a new Todo')
+  end
 end
