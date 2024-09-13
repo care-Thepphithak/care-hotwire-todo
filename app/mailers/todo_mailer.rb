d4017d38ca46e9e87a4f7f135573140afd1708ff
e3tow1c <thepphithak.w@kkumail.com>
2024-09-13 10:49:06 +0700

    add todo_passed_due_date mailer

--- app/mailers/todo_mailer.rb
+++ app/mailers/todo_mailer.rb
@@ -6,4 +6,11 @@ class TodoMailer < ApplicationMailer
     @user = todo.user
     mail(to: @user.email, subject: 'You have been assigned a new Todo')
   end
+
+  def todo_passed_due_date(todo)
+    Rails.logger.info "Sending email to #{todo.user.email}"
+    @todo = todo
+    @user = todo.user
+    mail(to: @user.email, subject: 'Your Todo has passed its due date :(')
+  end
 end
