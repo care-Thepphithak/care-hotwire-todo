d3b961009c25b3696b69c7e019d8f2f3db87fcb4
e3tow1c <thepphithak.w@kkumail.com>
2024-09-17 22:36:13 +0700

    log the current time when running

--- app/workers/todo_due_checker_worker.rb
+++ app/workers/todo_due_checker_worker.rb
@@ -4,6 +4,7 @@ class TodoDueCheckerWorker
 
   def perform
     # 0 is the status for incomplete 'todo'
+    Rails.logger.info "====== TodoDueCheckerWorker running now at #{Time.now} ======"
     Todo.where('due_date < ? AND status = ?', Time.now, 0).find_each do |todo|
       TodoMailer.todo_passed_due_date(todo).deliver_later
     end
