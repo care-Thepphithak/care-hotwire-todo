caacfde20e03bd9fcfc4602949110a1be0f34584
e3tow1c <thepphithak.w@kkumail.com>
2024-09-18 00:16:36 +0700

    change to positional argument as rails suggest because the old way will be deprecated in Rails 8.0

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,10 +1,8 @@
 class Todo < ApplicationRecord
     belongs_to :user
     validates_presence_of :name
-    enum status: {
-        incomplete: 0,
-        complete: 1
-    }
+    enum :status, { incomplete: 0, complete: 1 }
+
 
     after_create :send_assignment_email
 
