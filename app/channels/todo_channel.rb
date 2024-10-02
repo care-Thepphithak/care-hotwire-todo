6d3e35cf167f59556fe575aa245765b8d0984d7e
e3tow1c <thepphithak.w@kkumail.com>
2024-10-02 17:37:51 +0700

    when a consumer subscribed let it stream from todo_channel

--- app/channels/todo_channel.rb
+++ app/channels/todo_channel.rb
@@ -1,9 +1,13 @@
 class TodoChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "todo_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  def retrieve_todos
+    ActionCable.server.broadcast 'todo_channel', { todos: Todo.all }
   end
 end
