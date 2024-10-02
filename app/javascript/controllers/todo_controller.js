12c345ce4f34786147c8334f5b38c453bd863541
e3tow1c <thepphithak.w@kkumail.com>
2024-10-02 17:49:26 +0700

    use Action Cable for real-time updates

--- app/javascript/controllers/todo_controller.js
+++ app/javascript/controllers/todo_controller.js
@@ -1,7 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
+import consumer from "channels/consumer"
 
 // Connects to data-controller="todo"
 export default class extends Controller {
+  static targets = ["list", "incomplete", "complete"]
+
   connect() {
+    this.subscription = consumer.subscriptions.create("TodoChannel", {
+      received: this.updateTodos.bind(this)
+    })
+  }
+
+  disconnect() {
+    this.subscription.unsubscribe()
+  }
+
+  updateTodos(data) {
+    const isAtIncompleteTab = this.incompleteTarget.classList.contains("border-blue-600");
+    const todos = isAtIncompleteTab ? data.incomplete : data.complete;
+    this.listTarget.innerHTML = todos;
   }
 }
