f52c7748d0f7583929a42846b27e741c6fd30ca5
e3tow1c <thepphithak.w@kkumail.com>
2024-10-03 01:03:50 +0700

    add receive method for broadcasting chat data

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -6,4 +6,8 @@ class ChatChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
+
+  def receive(data)
+    ActionCable.server.broadcast("chat_channel", data)
+  end
 end
