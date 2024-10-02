f06661b0d1ed3ff37797bf87a23f18ab458b6541
e3tow1c <thepphithak.w@kkumail.com>
2024-10-02 17:49:01 +0700

    set up action cable connection to indentified by current user which is now hard-coded as ENV["CABLE_EMAIL"]

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,18 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    def find_verified_user
+      verify_user = User.find_by(email: ENV['CABLE_EMAIL'])
+      if verify_user
+        verify_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
