071d07b841ef8e35362e9b1d2c047e50cceddb56
e3tow1c <thepphithak.w@kkumail.com>
2024-10-02 22:29:36 +0700

    Add chat room view

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     end
     patch :change_status, on: :member
   end
+
+  get 'chat', to: 'chat#index', as: :chat
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
